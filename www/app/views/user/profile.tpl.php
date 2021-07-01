fc49f84c4c973bfb338cfc6b5d883da4b9589069
Chetan Thumar <chetan.thumar@tiez.nl>
2021-07-01 12:07:24 +0530

    fix label error

--- www/app/views/user/profile.tpl.php
+++ www/app/views/user/profile.tpl.php
@@ -2,56 +2,56 @@
     <input type="hidden" name="_token" value="<?php echo $token ?>">
     <div class="col-md-4">
         <div class="input-box">
-            <select name="title" required>
+            <select id="title" name="title" required>
                 <option value=""><?php echo $lang['text_title']; ?></option>
                 <option value="Mr." <?php if ($user_data['title'] == 'Mr.') { echo "selected"; } ?>><?php echo $lang['text_mr']; ?></option>
                 <option value="Mrs." <?php if ($user_data['title'] == 'Mrs.') { echo "selected"; } ?>><?php echo $lang['text_mrs']; ?></option>
                 <option value="Ms." <?php if ($user_data['title'] == 'Ms.') { echo "selected"; } ?>><?php echo $lang['text_ms']; ?></option>
                 <option value="Miss." <?php if ($user_data['title'] == 'Miss.') { echo "selected"; } ?>><?php echo $lang['text_miss']; ?></option>
             </select>
-            <label><?php echo $lang['text_title']; ?></label>
+            <label for="title"><?php echo $lang['text_title']; ?></label>
         </div>
     </div>
     <div class="col-md-4">
         <div class="input-box">
-            <input type="text" name="firstname" pattern="[A-Z,a-z, ]*" value="<?php echo $user_data['firstname']; ?>" required>
-            <label><?php echo $lang['text_first_name']; ?></label>
+            <input type="text" id="firstname" name="firstname" pattern="[A-Z,a-z, ]*" value="<?php echo $user_data['firstname']; ?>" required>
+            <label for="firstname" ><?php echo $lang['text_first_name']; ?></label>
         </div>
     </div>
     <div class="col-md-4">
         <div class="input-box">
-            <input type="text" name="lastname" pattern="[A-Z,a-z, ]*" value="<?php echo $user_data['lastname']; ?>" required>
-            <label><?php echo $lang['text_last_name']; ?></label>
+            <input type="text" id="lastname" name="lastname" pattern="[A-Z,a-z, ]*" value="<?php echo $user_data['lastname']; ?>" required>
+            <label for="lastname"><?php echo $lang['text_last_name']; ?></label>
         </div>
     </div>
     <div class="col-md-4">
         <div class="input-box">
-            <input type="text" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" value="<?php echo $user_data['email']; ?>" readonly required>
-            <label><?php echo $lang['text_email_address']; ?></label>
+            <input type="text" id="email" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" value="<?php echo $user_data['email']; ?>" readonly required>
+            <label for="email"><?php echo $lang['text_email_address']; ?></label>
         </div>
     </div>
     <div class="col-md-4">
         <div class="input-box">
-            <input type="text" name="mobile" pattern="[0-9]*" value="<?php echo $user_data['mobile']; ?>" required>
-            <label><?php echo $lang['text_mobile_number']; ?></label>
+            <input type="text" id="mobile" name="mobile" pattern="[0-9]*" value="<?php echo $user_data['mobile']; ?>" required>
+            <label for="mobile"><?php echo $lang['text_mobile_number']; ?></label>
         </div>
     </div>
     <div class="col-md-4">
         <div class="input-box">
-            <input type="text" name="dob" value="<?php echo date_format(date_create($user_data['dob']), $siteinfo['date_format']); ?>" required>
-            <label><?php echo $lang['text_birthday']; ?></label>
+            <input type="text" id="dob" name="dob" value="<?php echo date_format(date_create($user_data['dob']), $siteinfo['date_format']); ?>" required>
+            <label for="dob"><?php echo $lang['text_birthday']; ?></label>
         </div>
     </div>
     <div class="col-md-4">
         <div class="input-box">
-            <select name="gender" required>
+            <select id="gender" name="gender" required>
                 <option value=""><?php echo $lang['text_gender']; ?></option>
                 <option value="Male" <?php if ($user_data['gender'] == 'Male') { echo "selected"; } ?>><?php echo $lang['text_male']; ?></option>
                 <option value="Female" <?php if ($user_data['gender'] == 'Female') { echo "selected"; } ?>><?php echo $lang['text_female']; ?></option>
                 <option value="Prefer not to disclose" <?php if ($user_data['gender'] == 'Prefer not to disclose') { echo "selected"; } ?>><?php echo $lang['text_prefer_not_to_disclose']; ?></option>
                 <option value="Other" <?php if ($user_data['gender'] == 'Other') { echo "selected"; } ?>><?php echo $lang['text_other']; ?></option>
             </select>
-            <label><?php echo $lang['text_gender']; ?></label>
+            <label for="gender"><?php echo $lang['text_gender']; ?></label>
         </div>
     </div>
     <!--div class="col-md-4">
@@ -101,32 +101,32 @@
     </div>
     <div class="col-md-4">
         <div class="input-box">
-            <input type="text" name="address[address1]" value="<?php echo $user_data['address']['address1']; ?>">
-            <label class="col-form-label"><?php echo $lang['text_address_line1']; ?></label>
+            <input type="text" id="address1" name="address[address1]" value="<?php echo $user_data['address']['address1']; ?>">
+            <label class="col-form-label" for="address1"><?php echo $lang['text_address_line1']; ?></label>
         </div>
     </div>
     <div class="col-md-4">
         <div class="input-box">
-            <input type="text" name="address[address2]" value="<?php echo $user_data['address']['address2']; ?>">
-            <label class="col-form-label"><?php echo $lang['text_address_line2']; ?></label>
+            <input type="text" id="address2" name="address[address2]" value="<?php echo $user_data['address']['address2']; ?>">
+            <label class="col-form-label" id="address2"><?php echo $lang['text_address_line2']; ?></label>
         </div>
     </div>
     <div class="col-md-4">
         <div class="input-box">
-            <input type="text" name="address[city]" value="<?php echo $user_data['address']['city']; ?>">
-            <label class="col-form-label"><?php echo $lang['text_city']; ?></label>
+            <input type="text" id="city" name="address[city]" value="<?php echo $user_data['address']['city']; ?>">
+            <label class="col-form-label" for="city"><?php echo $lang['text_city']; ?></label>
         </div>
     </div>
     <div class="col-md-4">
         <div class="input-box">
-            <input type="text" name="address[country]" value="<?php echo $user_data['address']['country']; ?>">
-            <label class="col-form-label"><?php echo $lang['text_country']; ?></label>
+            <input type="text" id="country" name="address[country]" value="<?php echo $user_data['address']['country']; ?>">
+            <label class="col-form-label" for="country"><?php echo $lang['text_country']; ?></label>
         </div>
     </div>
     <div class="col-md-4">
         <div class="input-box">
-            <input type="text" name="address[postal]" value="<?php echo $user_data['address']['postal']; ?>" maxlength="7" onkeypress="return alphaNumericValidation(event)">
-            <label class="col-form-label"><?php echo $lang['text_postal_code']; ?></label>
+            <input type="text" id="postal" name="address[postal]" value="<?php echo $user_data['address']['postal']; ?>" maxlength="7" onkeypress="return alphaNumericValidation(event)">
+            <label class="col-form-label" for="postal"><?php echo $lang['text_postal_code']; ?></label>
         </div>
     </div>
     <div class="col-12">
