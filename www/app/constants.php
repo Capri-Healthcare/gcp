5b706dc5c92f850da61a98a130b5f1ad6519b2ab
Chetan Thumar <chetan.thumar@tiez.nl>
2021-10-14 18:43:30 +0530

    Fix the text message issue

--- www/app/constants.php
+++ www/app/constants.php
@@ -36,8 +36,8 @@ define('APPOINTMENT_VIDEO_CONSULTATION_TYPE', 'video_consultation');
 define('DEFAULT_COUNTRY_CODE', '+44');
 
 // Text message
-define('TEXT_MSG_APPOINTMENT_BOOKING_CONFIRMATION', 'Dear {patient_name}, Confirmation of appointment on {appointment_time}. https://www.worcesterglaucoma.co.uk'. '<br>'. 'Regards,' .'<br>' . NAME . '.');
-define('TEXT_MSG_APPOINTMENT_BOOKING_CONFIRMATION_WITH_HOSTPITAL', 'Dear {patient_name}, Confirmation of appointment on {appointment_time} at {hospital_name}. For queries call {hospital_phone_number}. https://www.worcesterglaucoma.co.uk'. '<br>'. 'Regards,' .'<br>' . NAME . '.');
+define('TEXT_MSG_APPOINTMENT_BOOKING_CONFIRMATION', "Dear {patient_name},\r\n\r\nYour appointment with Dr Tarun Sharma has been booked on {appointment_time}. Click the link for the location details https://www.worcesterglaucoma.co.uk/private-consultations-with-mr-tarun-sharma \r\n\r\nRegards,\r\n" . NAME);
+define('TEXT_MSG_APPOINTMENT_BOOKING_CONFIRMATION_WITH_HOSTPITAL', "Dear {patient_name},\r\n\r\nYour appointment with Dr Tarun Sharma has been booked on {appointment_time} at {hospital_name}. Check your email for more details. For queries call {hospital_phone_number}. Click the link for the location details https://www.worcesterglaucoma.co.uk/private-consultations-with-mr-tarun-sharma\r\n\r\nRegards,\r\n" . NAME);
 define('TEXT_MSG_APPOINTMENT_STATUS_UPDATE', 'Dear {patient_name}, Your appointment status update with ' . NAME . '.');
 define('TEXT_MSG_APPOINTMENT_REMINDER', 'Dear {patient_name}, This is a reminder from ' . NAME . ' about your appointment on {appointment_time}' . '.');
 define('TEXT_MSG_OPEN_CALL_INVITATION', 'Hello, This is {doctor_name}. Join me on video call: {video_consultation_link}');
