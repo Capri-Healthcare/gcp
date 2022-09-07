5e2e877da2ccaeb31172590b751dbeda1dc9bb33
Chetan Thumar <chetan.thumar@tiez.nl>
2022-09-07 08:30:38 +0530

    added optician address as CC

--- www/admin/app/http/models/Appointment.php
+++ www/admin/app/http/models/Appointment.php
@@ -1134,20 +1134,39 @@ class Appointment extends Model
         $body .= (!empty($about_doctor['position']) and !is_null($about_doctor['position'])) ? ("<br>" . $about_doctor['position']) : "";
         $body .= "</strong>";
 
-
-        $body .= "<br><br>";
         
         if(!empty($gp_address)){
+			$body .= "<br><br>";
             $body .= "<span style='font-size:12px'>";
             $body .= "CC:"."<br><br>";
             $body .= (!empty($gp_name)) ? ('<strong>'.$gp_name.'</strong><br>') : '';
+			if(!empty($gp_address)){
             $referee_address_arr = explode(',', str_replace(', ', ',', $gp_address));
-            foreach($referee_address_arr as $address){
-                $body .= ucfirst(strtolower($address))."<br>";
-            }
+				foreach($referee_address_arr as $address){
+					$body .= ucfirst(strtolower($address))."<br>";
+				}
+			}
             $body .= (!empty($appointment['gp_postal_code'])) ? ($appointment['gp_postal_code']) : "";
             $body .= "</span>";
         }
+		
+        $referee_name = (!empty($appointment['referee_name']) and !is_null($appointment['referee_name'])) ? $appointment['referee_name'] : "";
+        $referee_address = (!empty($appointment['referee_address']) and !is_null($appointment['referee_address'])) ? $appointment['referee_address'] : "";
+        if(!empty($referee_name)){
+			$body .= "<br><br>";
+            $body .= "<span style='font-size:12px'>";
+            $body .= "CC:"."<br><br>";
+            $body .= (!empty($referee_name)) ? ('<strong>'.$referee_name.'</strong><br>') : '';
+			if(!empty($referee_address)){
+				$referee_address_arr = explode(',', str_replace(', ', ',', $referee_address));
+				foreach($referee_address_arr as $address){
+					$body .= ucfirst(strtolower($address))."<br>";
+				}
+			}
+            $body .= "</span>";
+        }
+		
+		
         $body .= "</div>";
 
         // echo $body;exit;
